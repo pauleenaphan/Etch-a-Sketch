2f1dca12cd95c8bba7e80a072314bd0fdf180e83
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-04-03 23:40:57 -0700

    added etchig mechanic

--- script.js
+++ script.js
@@ -3,15 +3,29 @@ const container = document.querySelector('#grid');
 //We loop 16 times bc we want 16x16
 for(let i = 0; i < 16; i++){
     //This will create each individual row
-    const row = document.createElement('div');
+    var row = document.createElement('div');
+    row.className = 'row';
     for(let i = 0; i < 16; i++){
         //Creates each row then add it to the grid
         var square = document.createElement('div');
-        square.className = 'square'
+        //square.setAttribute('id', 'square');
+        square.className = "square";
         row.appendChild(square);
         container.appendChild(row);
     }
 
 }
 
+var sqr = document.getElementsByClassName("square");
+
+//When a sqr is being hovered over make it red
+//evt refers to the sqr being hovered over on
+function colorFunc(evt){
+    evt.target.style.backgroundColor = "red";
+}
+
+//We need to loop through to add eventlis to all the squares
+for(var i = 0; i < sqr.length; i++){
+    sqr[i].addEventListener('mouseover', colorFunc);
+}
 
