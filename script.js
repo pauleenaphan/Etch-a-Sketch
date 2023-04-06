b16ad94144c7b7ed7e2a3fa7eebb30af244cd2a9
Pauleena Phan <pauleenaphan@Pauleenas-MacBook-Pro.local>
2023-04-06 15:44:40 -0700

    added change color to btn

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ grid.className = 'gridoutline';
 var gridbtn = document.getElementById('gridsizebtn');
 gridbtn.addEventListener('click', createGrid);
 
-//generates the grid 
+//Declare a grid on load page 
+//Also helps so that if the user highlighted before making a grid the grid won't reset color
+for(let i = 0; i < 10; i++){
+    var row = document.createElement('div');
+    row.className = 'row'
+    for(let i = 0; i < 10; i++){
+        var square = document.createElement('div');
+        square.className = "square";
+        row.appendChild(square);
+        grid.appendChild(row);
+    }
+}
+container.appendChild(grid);
+
+//generates the grid based on the user input
 function createGrid(){
     const sqrs = Array.from(document.getElementsByClassName('row'));
 
@@ -17,6 +31,10 @@ function createGrid(){
     })
 
     var size = prompt("How big do you want your grid? (max size is 100)");
+
+    while(size > 100){
+        size = prompt("Please enter a value less than 100.");
+    }
     for(let i = 0; i < size; i++){
         //This will create each individual row
         var row = document.createElement('div');
@@ -37,13 +55,14 @@ function createGrid(){
 
 container.appendChild(grid);
 
+
 var sqr = document.getElementsByClassName("gridoutline");
 
 //When a sqr is being hovered over make it red
 //evt refers to the sqr being hovered over on
 function colorFunc(evt){
-    console.log("test");
-    evt.target.style.backgroundColor = "yellow";
+    //gets the current value of colors that allows the user to draw with this color
+    evt.target.style.backgroundColor = document.getElementById('colors').value;
 }
 
 //We need to loop through to add eventlis to all the squares
