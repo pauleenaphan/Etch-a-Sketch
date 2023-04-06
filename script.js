86b7a9d8588c4d76a21e99367ff677a00714f222
Pauleena Phan <pauleenaphan@Pauleenas-MacBook-Pro.local>
2023-04-06 12:38:15 -0700

    Added color function

--- script.js
+++ script.js
@@ -3,59 +3,47 @@ const container = document.querySelector('#grid');
 var grid = document.createElement('div');
 grid.className = 'gridoutline';
 
-
+//button for grid size
 var gridbtn = document.getElementById('gridsizebtn');
 gridbtn.addEventListener('click', createGrid);
 
-
+//generates the grid 
 function createGrid(){
-   const sqrs = Array.from(document.getElementsByClassName('row'));
+    const sqrs = Array.from(document.getElementsByClassName('row'));
 
-   sqrs.forEach(i =>{
-    i.classList.remove('row');
-   })
+    //removes the previous div we created to create a new grid
+    sqrs.forEach(i =>{
+        i.remove('div');
+    })
+
     var size = prompt("How big do you want your grid? (max size is 100)");
     for(let i = 0; i < size; i++){
         //This will create each individual row
         var row = document.createElement('div');
         row.className = 'row';
         for(let i = 0; i < size; i++){
-            //Creates each row then add it to the grid
+            //Creates each row by adding squares then add it to the grid
             var square = document.createElement('div');
-            //square.setAttribute('id', 'square');
             square.className = "square";
+
+            //adding each sqr to the row
             row.appendChild(square);
-            grid.appendChild(row);
+            
         }
+        //adding each row to the grid 
+        grid.appendChild(row);
     }
 }
 
 container.appendChild(grid);
 
-
-/*
-//We loop 16 times bc we want 16x16
-for(let i = 0; i < 16; i++){
-    //This will create each individual row
-    var row = document.createElement('div');
-    row.className = 'row';
-    for(let i = 0; i < 16; i++){
-        //Creates each row then add it to the grid
-        var square = document.createElement('div');
-        //square.setAttribute('id', 'square');
-        square.className = "square";
-        row.appendChild(square);
-        container.appendChild(row);
-    }
-
-}
-*/
-var sqr = document.getElementsByClassName("square");
+var sqr = document.getElementsByClassName("gridoutline");
 
 //When a sqr is being hovered over make it red
 //evt refers to the sqr being hovered over on
 function colorFunc(evt){
-    evt.target.style.backgroundColor = "red";
+    console.log("test");
+    evt.target.style.backgroundColor = "yellow";
 }
 
 //We need to loop through to add eventlis to all the squares
